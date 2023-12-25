f7edfab855e4843cb23dfccb5d32122e63a86a18
ATTHAPONG <atthapong@efinancethai.com>
2023-12-25 21:36:41 +0700

    continue doing from p.boss

---
+++ Tutorial/Tutorial/Models/Register/RegisterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Tutorial.Models.Register
+{
+    public class RegisterDto
+    {
+        public string fullname { get; set; } = string.Empty;
+        public string username { get; set; } = string.Empty;
+        public string password { get; set; } = string.Empty;
+    }
+}
--- Tutorial/Tutorial/Services/USER/ServicesUser.cs
+++ Tutorial/Tutorial/Services/USER/ServicesUser.cs
@@ -1,30 +1,69 @@
-﻿using Tutorial.Models.Login;
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+using Tutorial.Models.Login;
+using Tutorial.Models.Register;
 
 namespace Tutorial.Services.USER
 {
+    /*public interface IServicesUser
+    {
+        Task<List<loginDtos>> getAllUser();
+        Task<loginDtos?> getUserByUsername(string username);
+        Task<RegisterDto?> setUserwithRegister(string username, string password, string fullname);
+    }*/
     public class ServicesUser : IServicesUser
     {
-        private readonly List<loginDtos> _user;
+        private List<loginDtos> _user = new List<loginDtos>()
+            {
+                    new loginDtos() { username = "test", password = "Test", fullname = "Mr.Test" },
+                    new loginDtos() { username = "a", password = "abc", fullname = "Mr.A" }
+            };
+
+        private readonly List<RegisterDto> _regis;
+
         public ServicesUser()
         {
-            _user = initialUser();
+            _regis = new List<RegisterDto>();
+            //_user = initialUser();
         }
-        private static List<loginDtos> initialUser()
+
+        //private static List<loginDtos> initialUser()
+        //{
+        //    var data
+        //    return data;
+        //}
+
+        public async Task<List<loginDtos>> getAllUser()
         {
-            var data = new List<loginDtos>()
-            {new loginDtos() { username = "test", password = "Test", fullname = "Mr.Test" },
-            new loginDtos() { username = "a", password = "abc", fullname = "Mr.A" }
-            };
-            return data;
+            return await Task.FromResult(_user);
         }
-        public Task<List<loginDtos>> getAllUser()
-        {
-            return Task.FromResult(_user);
-        }
-        public Task<loginDtos?> getUserByUsername(string username)
+
+        public async Task<loginDtos?> getUserByUsername(string username)
         {
             var data = _user.FirstOrDefault(x => x.username.ToLower() == username.ToLower());
-            return Task.FromResult(data);
+            return await Task.FromResult(data);
+        }
+
+        public async Task<RegisterDto?> setUserwithRegister(string username, string password, string fullname)
+        {
+            var data = new RegisterDto
+            {
+                username = username,
+                password = password,
+                fullname = fullname
+            };
+
+            _regis.Add(data);
+
+            var loginData = new loginDtos
+            {
+                username = username,
+                password = password,
+                fullname = fullname,
+            };
+
+            _user.Add(loginData);
+
+            return await Task.FromResult<RegisterDto?>(data);
         }
     }
 }
