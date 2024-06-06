a7c25223669fdb2e86037f430ecc94cda21d08de
Suphanat Boss <70587200+Suphanat-th@users.noreply.github.com>
2024-06-06 12:31:51 +0700

    Clear Solution
    Clear project

--- Tutorial/Tutorial/Models/Register/RegisterDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Tutorial.Models.Register
-{
-    public class RegisterDto
-    {
-        public string fullname { get; set; } = string.Empty;
-        public string username { get; set; } = string.Empty;
-        public string password { get; set; } = string.Empty;
-    }
-}
