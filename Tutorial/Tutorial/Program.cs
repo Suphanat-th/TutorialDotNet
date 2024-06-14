92f9d2ab10836e370da51ee036096ad7722f1461
GUN-PC\zbackup <kantapon.stk@gmail.com>
2024-06-14 21:39:10 +0700

    Change  Life Cycle Service DI

--- Tutorial/Tutorial/Program.cs
+++ Tutorial/Tutorial/Program.cs
@@ -4,8 +4,8 @@ using Tutorial.Services.USER;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation() ;
-builder.Services.AddScoped<IServicesUser, ServicesUser>();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+builder.Services.AddSingleton<IServicesUser, ServicesUser>();
 builder.Services.AddTransient<RegisterDto>();
 
 var app = builder.Build();
